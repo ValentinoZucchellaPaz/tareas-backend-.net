63a3ba322984034f92c2d5cca9b4672e27bfe8b8
Valentino Zucchella Paz <vzucchellapaz@gmail.com>
2024-11-06 01:23:51 -0300

    problema8reinas

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TareasC_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Tablero tablero = new Tablero();
+
+        if (tablero.Resolver())
+        {
+            Console.WriteLine("Solución encontrada:");
+            tablero.MostrarTablero();
+        }
+        else
+        {
+            Console.WriteLine("No se encontró una solución.");
+        }
+    }
+}
---
+++ Tablero.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareasC_
+{
+    internal class Tablero
+    {
+        public int[,] Casillas { get; private set; }
+
+        public Tablero()
+        {
+            Casillas = new int[8, 8];
+
+            // Llenar el tablero con ceros explícitamente
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Casillas[i, j] = 0;
+                }
+            }
+        }
+
+
+        private bool EsPosicionSegura (int fila, int columna)
+        {
+            // Verificar columna hacia arriba
+            for (int i = 0; i < fila; i++)
+            {
+                if (Casillas[i, columna] == 1)
+                    return false;
+            }
+
+            // Verificar diagonal superior izquierda
+            for (int i = fila, j = columna; i >= 0 && j >= 0; i--, j--)
+            {
+                if (Casillas[i, j] == 1)
+                    return false;
+            }
+
+            // Verificar diagonal superior derecha
+            for (int i = fila, j = columna; i >= 0 && j < 8; i--, j++)
+            {
+                if (Casillas[i, j] == 1)
+                    return false;
+            }
+
+            // Si pasó todas las verificaciones, la posición es segura
+            return true;
+        }
+
+        public bool Resolver(int fila = 0)
+        {
+            // salida de f recursiva
+            if (fila >= 8)
+                return true;
+
+            // Intentar colocar una reina en cada columna de la fila actual
+            for (int columna = 0; columna < 8; columna++)
+            {
+                if (EsPosicionSegura(fila, columna))
+                {
+                    // Coloca la reina en la posición segura
+                    Casillas[fila, columna] = 1;
+
+                    // Llama recursivamente para intentar colocar la siguiente reina en la siguiente fila
+                    if (Resolver(fila + 1))
+                        return true;
+
+                    // Si colocar la reina en esta posición no lleva a una solución, retrocede
+                    Casillas[fila, columna] = 0; // Quitar la reina (backtrack)
+                }
+            }
+
+            // Si no se puede colocar ninguna reina en esta fila, retorna false para retroceder
+            return false;
+        }
+
+        public int[,] GetCasillas()
+        {
+            return Casillas;
+        }
+
+        
+
+
+
+
+
+
+
+
+
+        public void ColocarReina(int fila, int columna)
+        {
+            if (fila >= 0 && fila < 8 && columna >= 0 && columna < 8)
+            {
+                Casillas[fila, columna] = 1; // Coloca una reina
+            }
+        }
+        public void QuitarReina(int fila, int columna)
+        {
+            if (fila >= 0 && fila < 8 && columna >= 0 && columna < 8)
+            {
+                Casillas[fila, columna] = 0; // Quita la reina
+            }
+        }
+        public void MostrarTablero()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write(Casillas[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
